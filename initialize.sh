43c36aa079c5300a906fb0807faa4018d13af386
Eugine Park <pkeugine@gmail.com>
2022-02-20 06:27:18 +0900

    feat: decrease user input part and red output message

--- initialize.sh
+++ initialize.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-echo -e "\033[31m adding neovim repository"
+echo -e "\033[31m adding neovim repository\033[0m"
 yes | sudo add-apt-repository ppa:neovim-ppa/stable
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # OK
-echo -e "\033[31m adding docker repository"
+echo -e "\033[31m adding docker repository\033[0m"
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt -y update
 sudo apt -y upgrade
 
-echo -e "\033[31m installing tree command"
+echo -e "\033[31m installing tree command\033[0m"
 sudo apt install -y tree
 
 if ! command -v git &> /dev/null
 then
-    echo -e "\033[31m installing git"
+    echo -e "\033[31m installing git\033[0m"
     sudo apt install -y git
 else
-    echo -e "\033[31m git exists. skipping git installation"
+    echo -e "\033[31m git exists. skipping git installation\033[0m"
 fi
 
-echo setting git configuration
+echo echo -e "\033[31m setting git configuration\033[0m"
 git config --global user.email "pkeugine@gmail.com"
 git config --global user.name "Eugine Park"
 git config --global credential.helper cache
@@ -31,46 +31,50 @@ git config --global credential.helper 'cache --timeout=36000000'
 
 if ! command -v curl &> /dev/null
 then
-    echo -e "\033[31m installing curl"
+    echo -e "\033[31m installing curl\033[0m"
     sudo apt install -y curl
-    sudo apt install -y git
 else
-    echo -e "\033[31m curl exists. skipping curl installation"
+    echo -e "\033[31m curl exists. skipping curl installation\033[0m"
 fi
 
+echo -e "\033[31m installing neovim\033[0m"
 sudo apt install -y neovim
-# install vim-plug
+
+echo -e "\033[31m installing vim-plug\033[0m"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
+sudo update-alternatives --config --auto vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo udate-alternatives --config vim
+sudo update-alternatives --config --auto vim
 
+echo -e "\033[31m installing net-tools\033[0m"
 sudo apt install -y net-tools
+echo -e "\033[31m installing ethtool\033[0m"
 sudo apt install -y ethtool
-sudo apt install -y openssh-server
+# TODO: check if openssh-server is installed
+# sudo apt install -y openssh-server
 
-echo -e "\033[31m installing openJDK version 11"
+echo -e "\033[31m installing openJDK version 11\033[0m"
 sudo apt install -y openjdk-11-jdk
 
-echo -e "\033[31m installing node.js version 10"
-sudo apt install nodejs
+echo -e "\033[31m installing node.js version 10\033[0m"
+sudo apt install -y nodejs
 
-echo -e "\033[31m installing npm"
-sudo apt install npm
+echo -e "\033[31m installing npm\033[0m"
+sudo apt install -y npm
 
-echo -e "\033[31m installing yarn"
+echo -e "\033[31m installing yarn\033[0m"
 sudo npm install --global yarn
-# 여러 node 버전을 다운로드하고 관리하기 위해 nvm 설치
-echo -e "\033[31m installing nvm"
+
+echo -e "\033[31m installing nvm\033[0m"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
-echo -e "\033[31m installing node.js v16.13.2"
+echo -e "\033[31m installing node.js v16.13.2\033[0m"
 nvm install v16.13.2
 
-echo -e "\033[31m installing docker"
+echo -e "\033[31m installing docker\033[0m"
 sudo apt install -y docker-ce
 
 # sudo systemctl enable docker
